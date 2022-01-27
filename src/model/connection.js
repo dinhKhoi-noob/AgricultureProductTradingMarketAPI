8cf96d01c8e0d2e4646fde48e726cc99bbac4a02
dinhKhoi-noob <khoip1305@gmail.com>
2022-01-27 20:55:07 +0700

    Begin to config sequelize

--- src/model/connection.js
+++ src/model/connection.js
@@ -1,21 +1,21 @@
-let mysql = require('mysql2');
+let mysql = require("mysql2");
 require("dotenv").config();
-const {DB_HOST, DB_USERNAME, DB_PASSWORD} = process.env;
+const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
 
 let connection = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USERNAME,
-    password: DB_PASSWORD,
-    port: 3306,
-    database: "apm_db"
+  host: DB_HOST,
+  user: DB_USERNAME,
+  password: DB_PASSWORD,
+  port: 3306,
+  database: "apm_db",
 });
 
 connection.connect(function (err) {
-    if (err) {
-        console.log(err);
-        return;
-    };
-    console.log('Connected to database!');
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Connected to database!");
 });
 
 module.exports = connection;
